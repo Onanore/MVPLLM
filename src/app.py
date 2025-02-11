bedf4b7561a1f83c28e8d8a4d9a93f037a38741d
Onanore <shokanovd@inbox.ru>
2025-02-11 16:17:26 +0500

    Add files via upload

---
+++ src/app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.llms.ollama import Ollama
+
+from database import *
+from queries import query_with_rag_fusion, query_rag
+from embedding import get_embedding
+
+st.set_page_config(page_title="Ollama Chatbot", layout="wide")
+
+st.title("🤖 Local Chatbot with Ollama")
+st.markdown("Upload the file and ask questions based on it.")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+Ollama_model = st.sidebar.selectbox("Model: ",["deepseek-r1:1.5b", "deepseek-r1", "qwen2.5:1.5b"], "qwen2.5:1.5b")
+temperature = st.sidebar.slider("temperature", 0.0, 1.0, 0.3)
+uploaded_file = st.sidebar.file_uploader("Attach TXT file", type=["txt"])
+rag_fusion = st.sidebar.checkbox("RAG Fusion")
+
+if uploaded_file:
+    documents = load_documents(uploaded_file, uploaded_file.name)
+    chunks = split_documents(documents)
+    add_to_chroma(chunks)
+    st.success("File is uploaded!")
+
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+prompt = st.chat_input("Question...")
+if prompt:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    if rag_fusion:
+        answer = query_with_rag_fusion(prompt, Ollama_model, temperature)
+    else:
+        answer = query_rag(prompt, Ollama_model, temperature)
+
+    st.session_state.messages.append({"role": "assistant", "content": answer})
+    with st.chat_message("assistant"):
+        st.markdown(answer)
